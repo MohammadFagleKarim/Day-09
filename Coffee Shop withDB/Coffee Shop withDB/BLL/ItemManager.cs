ed12998f8eee897351452666acb0fa485934742b
Mohammad Fazle Karim <mohammadfaglekarim@gmail.com>
2019-12-26 12:30:18 -0800

    CoffeeShopWithDBUpdated

---
+++ Coffee Shop withDB/Coffee Shop withDB/BLL/ItemManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coffee_Shop_withDB.Repository;
+using System.Data;
+
+namespace Coffee_Shop_withDB.BLL
+{
+    public class ItemManager
+    {
+        ItemsRepository _itemsRepository = new ItemsRepository();
+
+        public bool ItemsAdd(string itemName, double price)
+        {
+            return _itemsRepository.ItemsAdd(itemName, price);
+        }
+
+        public bool IsNameExists(string itemsName)
+        {
+            return _itemsRepository.IsNameExists(itemsName);
+        }
+
+        public bool Delete(int itemId)
+        {
+            return _itemsRepository.Delete(itemId);
+        }
+
+        public bool Update(string itemName, double itemPrice, int itemId)
+        {
+            return _itemsRepository.Update(itemName, itemPrice, itemId);
+        }
+
+        public DataTable Display()
+        {
+            return _itemsRepository.Display();
+        }
+
+        public DataTable Search(string itemName)
+        {
+            return _itemsRepository.Search(itemName);
+        }
+    }
+}
---
+++ Coffee Shop withDB/Coffee Shop withDB/Repository/ItemsRepository.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Coffee_Shop_withDB.Repository
+{
+    public class ItemsRepository
+    {
+        public bool ItemsAdd(string itemName, double price)
+        {
+            bool isAdded = false;
+
+            try
+            {
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"INSERT INTO Items (ItemName, Price) VALUES ('" + itemName + "', " + price + ")";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+
+                //Execute
+                conn.Open();
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    isAdded = true;
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+            return isAdded;
+        }
+
+        public bool IsNameExists(string itemsName)
+        {
+            bool isExists = false;
+            try
+            {
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"SELECT * FROM Items WHERE ItemName = '" + itemsName + "' ";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+                //Execute
+                conn.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    isExists = true;
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+            return isExists;
+        }
+
+        public bool Delete(int itemId)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"DELETE FROM Items WHERE ItemId = '" + itemId + "' ";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+                //Execute
+                conn.Open();
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    return true;
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool Update(string itemName, double itemPrice, int itemId)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"UPDATE Items SET ItemName = '" + itemName + "', Price = " + itemPrice + " WHERE ItemId = " + itemId + "";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+                //Execute
+                conn.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    return true;
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+            return false;
+        }
+
+        public DataTable Display()
+        {
+            //try
+            //{
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"SELECT * FROM Items";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+                //Execute
+                conn.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                //if (dataTable.Rows.Count > 0)
+                //{
+                //    showDataGridView.DataSource = dataTable;
+                //}
+                //else
+                //    MessageBox.Show("Data not found");
+
+                conn.Close();
+            //}
+            //catch (Exception exeption)
+            //{
+            //    MessageBox.Show(exeption.Message);
+            //}
+
+            return dataTable;
+        }
+
+        public DataTable Search(string itemName)
+        {
+            //try
+            //{
+                //Connection
+                string connectionString = @"Server=DESKTOP-0LIAG2C\SQLEXPRESS; DataBase=CoffeeShop; Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //Command
+                string commandString = @"SELECT * FROM Items WHERE ItemName = '" + itemName + "' ";
+                SqlCommand sqlCommand = new SqlCommand(commandString, conn);
+
+                //Execute
+                conn.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                //if (dataTable.Rows.Count > 0)
+                //{
+                //    showDataGridView.DataSource = dataTable;
+                //}
+                //else
+                //    MessageBox.Show("Data not found");
+
+                //Close
+                conn.Close();
+
+            //}
+            //catch (Exception exeption)
+            //{
+            //    MessageBox.Show(exeption.Message);
+            //}
+
+            return dataTable;
+        }
+    }
+}
